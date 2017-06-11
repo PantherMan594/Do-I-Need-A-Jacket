6e64e296311e143ecc0dd9e38213aeed2c006b05
PantherMan594 <me@pantherman594.com>
2017-06-10 21:42:25 -0400

    Add login button

--- index.js
+++ index.js
@@ -60,3 +60,11 @@ function processQuery(data) {
         $('#searchResp ul').append('<li><a href="javascript:void(0);" onclick="query(\'' + entry.l + '\')">' + entry.name + '</a></li>')
     }
 }
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
