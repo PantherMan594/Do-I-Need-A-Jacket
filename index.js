9c9b5f1f8b1e2560599399d1457d0fbb2efb0467
PantherMan594 <me@pantherman594.com>
2017-06-10 20:43:21 -0400

    Add recommendation

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var queryResp = '/q/zmw:02108.1.99999';
+var coat = 36;
+var jacket = 45;
+var sweater = 60;
 
 $(document).ready(function () {
     $.ajax({
         type: 'GET',
-        url: 'http://api.wunderground.com/api/dba29ab1640cac5b/hourly' + queryResp + '.json',
+        url: 'https://api.wunderground.com/api/dba29ab1640cac5b/hourly' + queryResp + '.json',
         dataType: 'json',
         success: function(data) {processData(data);}
     });
@@ -11,11 +14,25 @@ $(document).ready(function () {
 
 function processData(data) {
     var entries = data.hourly_forecast;
-    var temp = entries[0].temp.english;
     var feels = entries[0].feelslike.english;
-    $('#temp').text(temp);
-    $('#feels').text(feels);
-    /*for (var i = 0; i < entries.length; i++) {
-
-    }*/
+    var feelsMin = feels;
+    for (var i = 1; i < 12; i++) {
+        if (feelsMin > entries[i].feelslike.english) {
+            feelsMin = entries[i].feelslike.english;
+        }
+    }
+    var simp = 'No';
+    var rec = 'you don\'t need a jacket';
+    if (feelsMin < coat) {
+        simp = 'Yes';
+        rec = 'it\'s cold out today, you need a coat';
+    } else if (feelsMin < jacket) {
+        simp = 'Yes';
+        rec = 'you should wear a jacket today';
+    } else if (feelsMin < sweater) {
+        simp = 'No';
+        rec = 'you only need a sweater';
+    }
+    $('title').text('Do I Need a Jacket? ' + simp + '.');
+    $('#rec').text(simp + ', ' + rec + '. The coldest it will feel today is ' + feelsMin + ' F.');
 }
